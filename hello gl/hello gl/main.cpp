1a48ac61daf66873378cc180f030267a0834c3aa
ebaah <ebaah46@yahoo.com>
2023-09-06 23:08:48 +0300

    hello rectangle: added double triangle to form a rectangle

--- hello gl/hello gl/main.cpp
+++ hello gl/hello gl/main.cpp
@@ -62,9 +62,14 @@ int main(int argc, const char * argv[]) {
     
     
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left
+    };
+    unsigned int indices[] = {
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
     };
     
     unsigned int vertexShader;
@@ -108,20 +113,26 @@ int main(int argc, const char * argv[]) {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
     
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     
     glBindVertexArray(VAO);
     
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER,0); //unbind
+//    glBindBuffer(GL_ARRAY_BUFFER,0); // do not unbind while VAO is active becauce VAO registers the last bound buffer
     glBindVertexArray(0); //unbind
+    
+//    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     
     // render loop
     while(!glfwWindowShouldClose(window)){
@@ -134,7 +145,7 @@ int main(int argc, const char * argv[]) {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES,0,3);
+        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT, 0);
         
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -144,6 +155,7 @@ int main(int argc, const char * argv[]) {
     std::cout<<"Freeing resources....."<<std::endl;
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteShader(shaderProgram);
     
     glfwTerminate();
